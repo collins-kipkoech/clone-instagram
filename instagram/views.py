6185836e51b6fd19b491c26031c4573411cf0a35
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-26 18:01:48 +0300

    created templates folder for rendering app information

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Image
+
 
 # Create your views here.
+def home_view(request):
+    posts = Image.objects.all()
+    context = {
+        'title':'home',
+        'posts':posts,
+
+    }
+    return render(request,'instagram/home.html',context)
+
+def image_details(request):
+    context = {
+        'title':'details',
+    }
+    return render(request,'instagram/image_details.html')
